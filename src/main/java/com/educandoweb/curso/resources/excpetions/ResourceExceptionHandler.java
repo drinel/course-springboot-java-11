3d23ce07008020d6c10121f1dcb486e7a1e45551
William Alex <will.i.am.002@hotmail.com>
2020-09-16 15:28:17 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/curso/resources/excpetions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/curso/resources/excpetions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.curso.services.exceptions.DatabaseException;
 import com.educandoweb.curso.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -18,6 +19,14 @@ class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource Not Found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError serr = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(serr);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError serr = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(serr);
 	}
